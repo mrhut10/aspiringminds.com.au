0ee0bd6c913c9d4aad59cef5398d0e18d42681fc
Luke Bennett <luke.bennett88@gmail.com>
2019-01-30 23:22:25 +1100

    Style 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,12 +7,12 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="flex flex-col items-center justify-center leading-normal py-16 text-center">
+    <div className="flex flex-1 h-full items-center justify-center text-center w-full">
       <Link className="no-underline" to="/">
         <h1 className="font-condensed text-grey-darkest text-5xl uppercase">
           404: Page not found
         </h1>
-        <p className="font-bold no-underline text-pink-dark text-lg">
+        <p className="font-bold no-underline text-teal-dark text-lg">
           Click to return to the home page
         </p>
       </Link>
