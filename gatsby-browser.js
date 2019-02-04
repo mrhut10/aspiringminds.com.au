999ff89f19fb293f6746655b8ba7866674679ba1
Luke Bennett <luke.bennett88@gmail.com>
2019-02-04 12:09:02 +1100

    Add fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 // Self host fonts using Typefaces.js https://github.com/KyleAMathews/typefaces
+
+// Load Poppins typeface
+require('typeface-poppins');
+
+// Load Nunito typeface
+require('typeface-nunito');
