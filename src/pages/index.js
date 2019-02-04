0c7510ae26c8bfc83e422af016c2f143e795550c
Luke Bennett <luke.bennett88@gmail.com>
2019-02-04 21:45:29 +1100

    Layout homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SEO from '../components/seo';
 import Nav from '../components/header';
+import Image from '../images/back-to-school.svg';
+import Mail from '../images/mail.svg';
 
 const IndexPage = () => (
-  <div className="font-sans">
-    <div className="h-screen max-w-4xl p-4 md:p-12 w-full">
-      <div className="index bg-no-repeat bg-left bg-cover bg-blue-0 flex flex-1 flex-wrap h-full items-center rounded-lg shadow-lg w-full">
-        <div className="max-w-xs mx-auto md:w-1/3 w-full">
+  <div className="font-sans h-screen">
+    <div className="fixed pin-t pin-r pin-l">
+      <Nav />
+    </div>
+    <div className="h-full max-w-4xl mt-20 md:mt-6 px-4 md:p-12 w-full">
+      <div className="index bg-no-repeat bg-left bg-cover bg-blue-0 flex flex-1 flex-wrap h-full items-center py-8 rounded-lg shadow-lg w-full">
+        <div className="leading-normal max-w-md md:mt-0 mx-auto px-4 md:px-8 md:w-1/2 w-full">
           <h1 className="text-5xl">Call to Action!</h1>
           <p>Stuff about Aspiring Minds should go in here ;-)</p>
           <p>
@@ -15,17 +20,24 @@ const IndexPage = () => (
             vitae rem. Ex labore voluptas, a soluta nesciunt consequatur dolores
             libero!
           </p>
-          <p>
-            <button>I'm a button</button>
+          <p className="flex justify-center md:justify-start mt-8">
+            <a
+              href="mailto:mryals@aspiringminds.com.au"
+              className="bg-blue-5 hover:bg-blue-4 flex items-center justify-center font-bold font-display inline-block px-4 py-1 no-underline rounded-full shadow text-white text-base tracking-wide uppercase"
+            >
+              <span>
+                <img className="h-6 mr-2 mt-1 w-6" src={Mail} alt="" />
+              </span>
+              <span>Contact Us</span>
+            </a>
           </p>
         </div>
-        <div className="flex items-center justify-center h-full max-w-md mx-auto md:w-2/3 w-full" />
+        <div className="flex items-center justify-center max-w-md mx-auto overflow-hidden px-4 md:px-8 md:w-1/2 w-full">
+          <img src={Image} alt="Woman kneeling down holding childs hands" />
+        </div>
       </div>
     </div>
-    <div className="max-w-lg mx-auto overflow-hidden rounded-lg max-w-3xl shadow-lg sticky pin-t w-full">
-      <Nav />
-    </div>
-    <div>HELLO</div>
+
     <SEO
       title="Home"
       keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
