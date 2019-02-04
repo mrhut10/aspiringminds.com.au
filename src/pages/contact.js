906761ef0f0019b226f689b2ae31261e1841baab
Luke Bennett <luke.bennett88@gmail.com>
2019-02-04 21:45:22 +1100

    Fix margin and padding

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
   return (
     <Layout>
       <div className="flex flex-wrap w-full">
-        <div className="w-full md:w-2/3">
-          <div className=" bg-blue-0 h-full m-4 my-0 px-4 py-8">
+        <div className="mb-4 md:mb-0 w-full md:w-2/3">
+          <div className="bg-blue-0 h-full m-4 my-0 px-4 py-8">
             <div className="max-w-sm mx-auto w-full">
               <h2 className="md:text-3xl">Info</h2>
             </div>
@@ -22,11 +22,11 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap-reverse w-full">
+      <div className="flex flex-wrap-reverse mb-4 w-full">
         <Form />
         <GetInTouch />
       </div>
-      <div className="flex items-center justify-center p-4 w-full">
+      <div className="flex items-center justify-center m-4 mt-0 md:mt-4">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.2376332534027!2d152.88546881514844!3d-31.435124281397854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b9dff1050581643%3A0x884f591e6dbfce6d!2s39+Clifton+Dr%2C+Port+Macquarie+NSW+2444!5e0!3m2!1sen!2sau!4v1549241524635"
           allowFullScreen
@@ -43,8 +43,8 @@ const Contact = () => {
 };
 
 const Form = () => (
-  <div className="mb-4 w-full md:w-1/2">
-    <div className=" bg-blue-9 h-full m-4 mb-8 md:mb-4 px-4 py-8 text-white">
+  <div className="mb-4 md:mb-0 w-full md:w-1/2">
+    <div className=" bg-blue-9 h-full m-4 md:mb-4 px-4 py-8 text-white">
       <div className="max-w-sm mx-auto w-full">
         <h2 className="md:text-3xl">How can we help?</h2>
         <FormConent />
@@ -54,7 +54,7 @@ const Form = () => (
 );
 
 const GetInTouch = () => (
-  <div className="mb-4 w-full md:w-1/2">
+  <div className="mb-4 md:mb-0 w-full md:w-1/2">
     <div className="bg-blue-0 h-full m-4 md:ml-0 px-4 py-8">
       <div className="leading-normal max-w-sm mx-auto text-xl w-full">
         <h2 className="md:text-3xl">Get in touch</h2>
