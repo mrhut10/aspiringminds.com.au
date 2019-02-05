80f094cd996705f65ddd7a19205645d41b4e9414
Luke Bennett <luke.bennett88@gmail.com>
2019-02-06 08:39:00 +1100

    Build out navigation

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const handleClick = ev => {
 const Header = () => (
   <header
     css={{ background: `hsla(360, 100%, 100%, .95)` }}
-    className="bg-white font-display pin-t sticky z-10"
+    className="bg-white border-b font-display pin-t sticky z-10"
   >
     <div className="md:flex w-full">
       <div className="flex items-center p-4">
@@ -47,8 +47,11 @@ const Header = () => (
         id="nav"
         className="hidden md:flex flex-1 md:items-center justify-end px-4 w-full md:w-auto"
       >
-        <NavLink link="/contact/" title="Contact" />
-        <NavLink link="/blog/" title="Blog" />
+        <NavLink link="/about" title="About" />
+        <NavLink link="/our-approach" title="Our approach" />
+        <NavLink link="/services" title="Services" />
+        <NavLink link="/contact" title="Contact" />
+        <NavLink link="/blog" title="Blog" />
       </nav>
     </div>
   </header>
@@ -57,7 +60,7 @@ const Header = () => (
 const NavLink = props => (
   <Link
     to={props.link}
-    className="block md:inline-block border-t md:border-0 font-bold no-underline px-6 py-4 uppercase text-grey-darkest"
+    className="block md:inline-block border-t md:border-0 font-bold no-underline px-6 py-4 text-grey-darkest"
   >
     {props.title}
   </Link>
---
+++ src/pages/our-approach.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const OurApproach = () => {
+  return (
+    <Layout>
+      <h1>Our approach</h1>
+    </Layout>
+  );
+};
+
+export default OurApproach;
