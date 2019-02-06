9024ffc47109202f9f707278f5006dbcf0ca1c2d
Luke Bennett <luke.bennett88@gmail.com>
2019-02-06 13:30:50 +1100

    Update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         background_color: '#63c5ff',
         theme_color: '#0043a4',
         display: 'minimal-ui',
-        icon: 'content/assets/icon.png',
+        icon: 'content/assets/icon.svg',
       },
     },
     // 'gatsby-plugin-offline',
