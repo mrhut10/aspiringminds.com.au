c09ac1a9a567d6974b65fd70943841ebb874bd2e
Luke Bennett <luke.bennett88@gmail.com>
2019-02-06 15:45:11 +1100

    Make body have blue background

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Footer from './footer.js';
 import '../css/tailwind.css';
 
 const Layout = ({ children }) => (
-  <div className="flex flex-col font-sans min-h-screen text-grey-darkest">
+  <div className="bg-white flex flex-col font-sans min-h-screen text-grey-darkest">
     <Header />
     <main className="flex flex-col flex-1 max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
       {children}
