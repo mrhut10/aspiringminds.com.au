00e599d0b8710ba616bcf34be7b7122bb5a0464e
Luke Bennett <luke.bennett88@gmail.com>
2019-02-06 21:58:12 +1100

    Add next section

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import About from '../../content/assets/about.svg';
+import Approach from '../../content/assets/approach.svg';
+import Blog from '../../content/assets/blog.svg';
 
 const Cards = () => {
   return (
-    <section className="cards">
-      <div>About</div>
-      <div>Our Approach</div>
-      <div>Blog</div>
+    <section className="cards w-full">
+      <div>
+        <Link to="/about">
+          <img src={About} alt="" />
+          <h2>About Us</h2>
+        </Link>
+      </div>
+      <div>
+        <Link to="/our-approach">
+          <img src={Approach} alt="" />
+          <h2>Our Approach</h2>
+        </Link>
+      </div>
+      <div>
+        <Link to="/blog">
+          <img src={Blog} alt="" />
+          <h2>Blog</h2>
+        </Link>
+      </div>
     </section>
   );
 };
