ce5988ab61fc3b9fd88b9a2979d9bb88d09fd3ce
Luke Bennett <luke.bennett88@gmail.com>
2019-02-06 13:32:07 +1100

    Add content to services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -5,6 +5,49 @@ const Services = () => {
   return (
     <Layout>
       <h1>Services</h1>
+      <ul>
+        <li>
+          Aspiring Minds provides for infant/primary aged children, generally in
+          the 4 to 12 year age group
+        </li>
+        <li>We cater for all levels of ability</li>
+        <li>
+          We provide educational intervention when this is recommended by our
+          health care professional collaborators
+        </li>
+        <li>
+          Rich Assessment Tasks enabling students to use and develop their
+          imagination and communication skills
+        </li>
+        <li>We provide individual, group and holiday programs</li>
+        <li>
+          Our programming is comprehensive, based on the NSW Curriculum (NESA)
+        </li>
+        <li>
+          Our approach is inclusive – our preference is to include all
+          stakeholders when preparing your child’s program (for example parents,
+          class teachers, speech and/or occupational therapist and relevant
+          other professionals)
+        </li>
+        <li>We teach all infant/primary subjects</li>
+
+        <li>We specialise in all areas of literacy and numeracy</li>
+
+        <li>
+          All staff have recognised qualifications and significant teaching
+          experience
+        </li>
+        <li>We are a one-stop-shop for all your child’s educational needs</li>
+
+        <li>
+          Our staff are all qualified and up-to-date with current curriculum
+          requirements and technology
+        </li>
+        <li>
+          We deliver differentiated wholistic programs taking each student’s
+          social, emotional and intellectual level into account
+        </li>
+      </ul>
     </Layout>
   );
 };
