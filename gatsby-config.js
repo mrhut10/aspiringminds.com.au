e46e2bbb67763ca89082a57c5683381861337621
Luke Bennett <luke.bennett88@gmail.com>
2019-02-07 07:43:11 +1100

    Revert to using backticks in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,81 +1,81 @@
 module.exports = {
   siteMetadata: {
-    title: 'Aspiring Minds',
-    author: 'Meredith Ryals',
-    description: 'Motivate, Educate, Elevate.',
-    // siteUrl: 'https://www.aspiringminds.com.au',
+    title: `Aspiring Minds`,
+    author: `Meredith Ryals`,
+    description: `Motivate, Educate, Elevate.`,
+    // siteUrl: `https://www.aspiringminds.com.au`,
     social: {
-      facebook: 'aspiringminds',
+      facebook: `aspiringminds`,
     },
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: `blog`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: 'assets',
+        name: `assets`,
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-postcss',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
     {
-      resolve: 'gatsby-plugin-purgecss',
+      resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        purgeOnly: ['src/css/style.css'], // Purge only tailwind
+        purgeOnly: [`src/css/style.css`], // Purge only tailwind
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: 'ADD YOUR TRACKING ID HERE',
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    // 'gatsby-plugin-feed',
+    // `gatsby-plugin-feed`,
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Aspiring Minds',
-        short_name: 'Aspiring Minds',
-        start_url: '/',
-        background_color: '#63c5ff',
-        theme_color: '#0043a4',
-        display: 'minimal-ui',
-        icon: 'content/assets/icon.svg',
+        name: `Aspiring Minds`,
+        short_name: `Aspiring Minds`,
+        start_url: `/`,
+        background_color: `#63c5ff`,
+        theme_color: `#0043a4`,
+        display: `minimal-ui`,
+        icon: `content/assets/icon.svg`,
       },
     },
-    // 'gatsby-plugin-offline',
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-remove-serviceworker`,
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
   ],
 };
