85fddbdaffe2ab658cb6fb7f33e6b57cb41156d3
Luke Bennett <luke.bennett88@gmail.com>
2019-02-07 12:31:05 +1100

    Break cards up into smaller components

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,26 +7,42 @@ import Blog from '../../content/assets/blog.svg';
 const Cards = () => {
   return (
     <section className="cards w-full">
-      <div>
-        <Link to="/about">
-          <img src={About} alt="" />
-          <h2>About Us</h2>
-        </Link>
-      </div>
-      <div>
-        <Link to="/our-approach">
-          <img src={Approach} alt="" />
-          <h2>Our Approach</h2>
-        </Link>
-      </div>
-      <div>
-        <Link to="/blog">
-          <img src={Blog} alt="" />
-          <h2>Blog</h2>
-        </Link>
-      </div>
+      <Card
+        link="/about"
+        img={About}
+        title="About Us"
+        body="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, ipsam
+      nesciunt libero quod enim dolorum id esse alias harum assumenda velit
+      accusamus ad, corporis magni et tenetur aliquam expedita eius!"
+      />
+      <Card
+        link="/our-approach"
+        img={Approach}
+        title="Our Approach"
+        body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos sequi
+        omnis at, numquam totam alias similique debitis nesciunt, dolorum
+        error ex excepturi nihil. Magnam libero aspernatur dolores animi,
+        laborum accusantium?"
+      />
+      <Card
+        link="/blog"
+        img={Blog}
+        title="Blog"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        fugit sapiente suscipit aliquam quibusdam eaque debitis, cum ratione
+        totam voluptates. Quaerat pariatur consequatur omnis nulla illum
+        sapiente eius voluptas harum!"
+      />
     </section>
   );
 };
+
+const Card = props => (
+  <Link className="flex-col p-4" to={props.link}>
+    <img className="h-48" src={props.img} alt="" />
+    <h2 className="mt-8 text-center">{props.title}</h2>
+    <p className="text-grey-darker">{props.body}</p>
+  </Link>
+);
 
 export default Cards;
