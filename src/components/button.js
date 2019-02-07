686b4dc9a44586e171b3b6d9de40e0528400ef33
Luke Bennett <luke.bennett88@gmail.com>
2019-02-07 14:25:19 +1100

    Add props to href in buttons

--- src/components/button.js
+++ src/components/button.js
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <p className="flex mt-8 w-full">
       <a
-        href="tel:0412522849"
+        href={props.link}
         className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white tracking-wide uppercase"
       >
         <span className="flex mr-2">
