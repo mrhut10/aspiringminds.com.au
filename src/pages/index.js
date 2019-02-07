12f4f0682e122dd8a54dc0eefbaa6169f1c34e22
Luke Bennett <luke.bennett88@gmail.com>
2019-02-07 12:32:48 +1100

    Continue laying out home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 import Header from '../components/header';
 import Hero from '../components/hero';
+import Skew from '../components/skew';
 import Cards from '../components/cards';
+import Button from '../components/button'
 import Footer from '../components/footer';
 import SEO from '../components/seo';
+
+import Phone from '../../content/assets/phone.svg'
 
 import '../css/tailwind.css';
 
@@ -16,7 +20,23 @@ const IndexPage = () => (
     />
     <Header />
     <Hero />
+    <Skew>
+      <div className="cards items-center justify-around text-center w-full">
+        <div>Motivate.</div>
+        <div>Educate.</div>
+        <div>Elevate.</div>
+      </div>
+    </Skew>
     <Cards />
+    <Skew>
+      <div className="text-xl">
+      <h2 className="text-5xl">Call us today for a free consultation</h2>
+      <Button
+          svg={Phone}
+          title="0412 522 849"
+          />
+      </div>
+    </Skew>
     <Footer />
   </div>
 );
