fb605e7cb46a5f534f01e9c561b609d42952d4a0
Luke Bennett <luke.bennett88@gmail.com>
2019-02-07 14:26:04 +1100

    Import SVG icons and add Contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import Header from '../components/header';
 import Hero from '../components/hero';
 import Skew from '../components/skew';
 import Cards from '../components/cards';
-import Button from '../components/button'
+import Button from '../components/button';
 import Footer from '../components/footer';
 import SEO from '../components/seo';
 
-import Phone from '../../content/assets/phone.svg'
+import Phone from '../../content/assets/phone.svg';
+import Mail from '../../content/assets/mail.svg';
 
 import '../css/tailwind.css';
 
@@ -30,11 +31,29 @@ const IndexPage = () => (
     <Cards />
     <Skew>
       <div className="text-xl">
-      <h2 className="text-5xl">Call us today for a free consultation</h2>
-      <Button
+        <h2 className="text-5xl">Contact us for any further enquires</h2>
+        <Button link="/contact" title="Contact" />
+      </div>
+    </Skew>
+    <Skew>
+      <div className="text-xl">
+        <h2 className="text-5xl">For more information, please email us</h2>
+        <Button
+          svg={Mail}
+          link="mailto:mryals@aspiringminds.com.au"
+          title="mryals@aspiringminds.com.au"
+        />
+      </div>
+    </Skew>
+    <br />
+    <Skew>
+      <div className="text-xl">
+        <h2 className="text-5xl">Call us today for a free consultation</h2>
+        <Button
           svg={Phone}
-          title="0412 522 849"
-          />
+          link="tel:0412522849"
+          title="0412&nbsp;522&nbsp;849"
+        />
       </div>
     </Skew>
     <Footer />
