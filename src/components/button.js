45e9d324c506de1e0842899af53ead2539f03880
Luke Bennett <luke.bennett88@gmail.com>
2019-02-08 22:09:30 +1100

    Update button component

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Button = (props) => {
+const Button = props => {
   return (
-    <p className="flex mt-8 w-full">
+    <p className="flex justify-center mt-8 w-full">
       <a
         href={props.link}
-        className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white tracking-wide uppercase"
+        className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white text-sm md:text-lg tracking-wide"
       >
         <span className="flex mr-2">
-          <img className="h-6 w-6" src={props.svg} alt="" />
+          <img className="h-4 md:h-6 w-4 md:w-6" src={props.svg} alt="" />
         </span>
         <span>{props.title}</span>
       </a>
