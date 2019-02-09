ddda7b49f78a4b24589647f1c4b569345e74e4c3
Luke Bennett <luke.bennett88@gmail.com>
2019-02-09 15:32:27 +1100

    Revert changes

--- blog/first-post.md
+++
@@ -1,9 +0,0 @@
----
-path: /blog/my-first-blog
-date: 2019-02-09
-title: First Blog
----
-
-# First post
-
-This is just a test post.
--- gatsby-config.js
+++ gatsby-config.js
@@ -24,10 +24,25 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        path: `${__dirname}/blog`,
-        name: 'markdown-pages',
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     },
     `gatsby-transformer-sharp`,
