c4e5aa76744c5cca6a36ffe6f1751460cd7be9e0
Luke Bennett <luke.bennett88@gmail.com>
2019-02-10 09:23:12 +1100

    Add testimonials component

---
+++ src/components/testimonials.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Testimonials = () => {
+  return (
+    <section className="px-4 py-16">
+      <h1 className="text-center">Testimonials</h1>
+      <div className="cards">
+        <div className="flex-col">
+          <h3>Some Guy</h3>
+          <div className="italic text-grey-darker">
+            <p>
+              "Meredith is a fantastic tutor, my son was failing math, but now
+              he's at the top of his class.
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                dolor adipisci necessitatibus, quod enim voluptate nihil
+                architecto quis nisi repudiandae autem cupiditate officiis! In
+                rem sequi, architecto obcaecati at nam?"
+              </p>
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-col">
+          <h3>Some Mum</h3>
+          <div className="italic text-grey-darker">
+            <p>
+              "Meredith is a fantastic tutor, my son was failing math, but now
+              he's at the top of his class.
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                dolor adipisci necessitatibus, quod enim voluptate nihil
+                architecto quis nisi repudiandae autem cupiditate officiis! In
+                rem sequi, architecto obcaecati at nam?"
+              </p>
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-col">
+          <h3>Someone Else</h3>
+          <div className="italic text-grey-darker">
+            <p>
+              "Meredith is a fantastic tutor, my son was failing math, but now
+              he's at the top of his class.
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                dolor adipisci necessitatibus, quod enim voluptate nihil
+                architecto quis nisi repudiandae autem cupiditate officiis! In
+                rem sequi, architecto obcaecati at nam?"
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
