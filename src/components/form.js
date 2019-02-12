db9edfc9b82424c94adbe5c7f13d3c40b03ba5c1
Luke Bennett <luke.bennett88@gmail.com>
2019-02-12 15:41:30 +1100

    Revert styling on form

--- src/components/form.js
+++ src/components/form.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { navigate } from "gatsby-link";
+import React from 'react';
+import { navigate } from 'gatsby-link';
 
 function encode(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 }
 
 export default class Index extends React.Component {
@@ -20,21 +20,22 @@ export default class Index extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
     })
-      .then(() => navigate(form.getAttribute("action")))
+      .then(() => navigate(form.getAttribute('action')))
       .catch(error => alert(error));
   };
 
   render() {
     return (
       <form
+        className="p-4 w-full"
         name="contact"
         method="post"
         action="/thanks/"
@@ -44,146 +45,67 @@ export default class Index extends React.Component {
       >
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
         <input type="hidden" name="form-name" value="contact" />
-        <div hidden>
+        <p hidden>
           <label>
-            Don’t fill this out:{" "}
+            Don’t fill this out:{' '}
             <input name="bot-field" onChange={this.handleChange} />
           </label>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor={"name"} >Your name</label>
-          <div className="control">
-            <input className="input" type={"text"} name={"name"} onChange={this.handleChange} id={"name"} required={true} />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor={"email"}>Email</label>
-            <div className="control">
-              <input className="input" type={"email"} name={"email"} onChange={this.handleChange} id={"email"} required={true} />
-            </div>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor={"message"}>Message</label>
-          <div className="control">
-            <textarea className="textarea" name={"message"} onChange={this.handleChange} id={"message"} required={true} />
-          </div>
-        </div>
-        <div className="field">
-          <button className="button is-link" type="submit">Send</button>
-        </div>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block font-display mb-2">Full name:</span>
+            <input
+              className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
+              type="text"
+              name="name"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block font-display mb-2">Phone number:</span>
+            <input
+              className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
+              type="text"
+              name="phone"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block font-display mb-2">Email address:</span>
+            <input
+              className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
+            <span className="block font-display mb-2">Message:</span>
+            <textarea
+              className="appearance-none bg-blue-0 block border h-48 leading-tight px-4 py-3 rounded-none text-base text-grey-darker w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
+              name="message"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p className="flex">
+          <button
+            className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white text-sm md:text-lg tracking-wide uppercase"
+            type="submit"
+          >
+            Send
+          </button>
+        </p>
       </form>
     );
   }
 }
-
-// import React from "react";
-// import { navigate } from "gatsby-link";
-
-// function encode(data) {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// }
-
-// export default class Index extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { isValidated: false };
-//   }
-
-//   handleChange = e => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.target;
-//     fetch("/", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//       body: encode({
-//         "form-name": form.getAttribute("name"),
-//         ...this.state
-//       })
-//     })
-//       .then(() => navigate(form.getAttribute("action")))
-//       .catch(error => alert(error));
-//   };
-
-//   render() {
-//     return (
-//       <form
-//         className="p-4 w-full"
-//         name="contact"
-//         method="post"
-//         action="/thanks/"
-//         data-netlify="true"
-//         data-netlify-honeypot="bot-field"
-//         onSubmit={this.handleSubmit}
-//       >
-//         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-//         <input type="hidden" name="form-name" value="contact" />
-//         <p hidden>
-//           <label>
-//             Don’t fill this out:{' '}
-//             <input name="bot-field" onChange={this.handleChange} />
-//           </label>
-//         </p>
-//         <p>
-//           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
-//             <span className="block font-display mb-2">Full name:</span>
-//             <input
-//               className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
-//               type="text"
-//               name="name"
-//               required
-//               onChange={this.handleChange}
-//             />
-//           </label>
-//         </p>
-//         <p>
-//           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
-//             <span className="block font-display mb-2">Phone number:</span>
-//             <input
-//               className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
-//               type="text"
-//               name="phone"
-//               required
-//               onChange={this.handleChange}
-//             />
-//           </label>
-//         </p>
-//         <p>
-//           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
-//             <span className="block font-display mb-2">Email address:</span>
-//             <input
-//               className="appearance-none bg-blue-0 block border leading-tight px-4 py-3 rounded-none text-base w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
-//               type="email"
-//               name="email"
-//               onChange={this.handleChange}
-//             />
-//           </label>
-//         </p>
-//         <p>
-//           <label className="block font-bold mb-6 text-xs tracking-wide uppercase">
-//             <span className="block font-display mb-2">Message:</span>
-//             <textarea
-//               className="appearance-none bg-blue-0 block border h-48 leading-tight px-4 py-3 rounded-none text-base text-grey-darker w-full focus:bg-white focus:border-blue-5 focus:outline-none hover:bg-white"
-//               name="message"
-//               required
-//               onChange={this.handleChange}
-//             />
-//           </label>
-//         </p>
-//         <p className="flex">
-//           <button
-//             className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white text-sm md:text-lg tracking-wide uppercase"
-//             type="submit"
-//           >
-//             Send
-//           </button>
-//         </p>
-//       </form>
-//     );
-//   }
-// }
