83e2930c843d8b080aafd4815dd7d76a1b36b6f2
Luke Bennett <luke.bennett88@gmail.com>
2019-02-12 15:36:52 +1100

    Try to get form working

---
+++ src/pages/thanks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Thanks = () => {
+  return (
+    <Layout>
+    <SEO
+        title="Message sent"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+    <div>
+    <h1>Message Sent</h1>
+      <p>
+        Thanks for reaching out to us. We will respond to you as soon as possible.
+      </p>
+    </div>
+    </Layout>
+  )
+}
+
+export default Thanks
