57be703c947ea5a725d71dfc203699b26d18d9ad
Luke Bennett <luke.bennett88@gmail.com>
2019-03-15 11:35:25 +1100

    Style services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Tagline from '../components/tagline';
+import Image from '../images/woman-and-child.svg';
 
 const Services = () => {
   return (
@@ -9,52 +11,86 @@ const Services = () => {
         title="Services"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
-      <h1>Services</h1>
-      <ul>
-        <li>
-          Aspiring Minds provides for infant/primary aged children, generally in
-          the 4 to 12 year age group
-        </li>
-        <li>We cater for all levels of ability</li>
-        <li>
-          We provide educational intervention when this is recommended by our
-          health care professional collaborators
-        </li>
-        <li>
-          Rich Assessment Tasks enabling students to use and develop their
-          imagination and communication skills
-        </li>
-        <li>We provide individual, group and holiday programs</li>
-        <li>
-          Our programming is comprehensive, based on the NSW Curriculum (NESA)
-        </li>
-        <li>
-          Our approach is inclusive – our preference is to include all
-          stakeholders when preparing your child’s program (for example parents,
-          class teachers, speech and/or occupational therapist and relevant
-          other professionals)
-        </li>
-        <li>We teach all infant/primary subjects</li>
-
-        <li>We specialise in all areas of literacy and numeracy</li>
-
-        <li>
-          All staff have recognised qualifications and significant teaching
-          experience
-        </li>
-        <li>We are a one-stop-shop for all your child’s educational needs</li>
-
-        <li>
-          Our staff are all qualified and up-to-date with current curriculum
-          requirements and technology
-        </li>
-        <li>
-          We deliver differentiated wholistic programs taking each student’s
-          social, emotional and intellectual level into account
-        </li>
-      </ul>
+      <div className="flex flex-wrap">
+        <div className="flex flex-col items-center justify-center max-w-sm mx-auto text-xl md:text-2xl w-full sm:w-1/2">
+          <div className="p-4 mb-8">
+            <h1>Services</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+              corporis aliquam labore ratione id autem rerum accusantium, nihil
+              laborum dolore facilis cum vero quisquam vel magnam reprehenderit
+              et debitis quaerat!
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center px-4 md:p-8 w-full sm:w-1/2">
+          <img src={Image} alt="" />
+        </div>
+      </div>
+      <div className="max-w-3xl mx-auto py-8">
+        <div className="services">
+          <Service title="First">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+              corrupti suscipit, aliquid cum in odio ea quidem quis optio
+              similique voluptas praesentium. Harum iure distinctio eius.
+              Numquam iure laudantium commodi?
+            </p>
+          </Service>
+          <Service title="Second">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem illo
+              magni eos maxime incidunt ea quibusdam suscipit voluptatum
+              impedit? A excepturi sunt eius laudantium ut, tempore quibusdam
+              assumenda repellat. Quia.
+            </p>
+          </Service>
+          <Service title="Third">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              nam blanditiis commodi, perferendis reprehenderit consequatur
+              voluptatem nulla facere a nisi illo maxime eum soluta. Aspernatur
+              velit iure rerum impedit quam?
+            </p>
+          </Service>
+        </div>
+        <div className="services">
+          <Service title="Fourth">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis ut
+              vitae est alias assumenda voluptatum quia neque deserunt soluta
+              accusamus, facilis sint ea nisi non corporis consectetur id
+              recusandae mollitia.
+            </p>
+          </Service>
+          <Service title="Fifth">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
+              explicabo alias animi quidem dolor similique dolores sed ipsum
+              neque, accusantium cum aperiam harum! Atque, dicta reprehenderit
+              nihil sint blanditiis nulla?
+            </p>
+          </Service>
+          <Service title="Sixth">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+              nemo voluptatibus quas rem iure rerum doloremque obcaecati omnis
+              adipisci quo a, odio autem reprehenderit reiciendis atque quasi,
+              nobis explicabo deserunt!
+            </p>
+          </Service>
+        </div>
+      </div>
+      <Tagline />
     </Layout>
   );
 };
+
+const Service = props => (
+  <div className="max-w-sm mx-auto px-4 py-8 w-full">
+    <h2>{props.title}</h2>
+    {props.children}
+  </div>
+);
 
 export default Services;
