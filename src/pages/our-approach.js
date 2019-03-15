dcf077352be6f40b79f3751a5ac5565b5f418867
Luke Bennett <luke.bennett88@gmail.com>
2019-03-15 12:42:00 +1100

    Update Our Approach layout

--- src/pages/our-approach.js
+++ src/pages/our-approach.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Tagline from '../components/tagline';
+
+import Image from '../images/approach.svg';
 
 const OurApproach = () => {
   return (
@@ -9,44 +12,86 @@ const OurApproach = () => {
         title="Our approach"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
-      <h1>Our Approach</h1>
-      <p>
-        At Aspiring Minds we recognise that our students are individuals and
-        therefore seek to:
-      </p>
-      <ul>
-        <li>
-          establish a positive deep-learning and inclusive environment in which
-          class members can be constructive as individual and group learners;
-        </li>
-        <li>
-          provide an opportunity for students to develop and maintain
-          self-esteem and a positive-growth mindset;
-        </li>
-        <li>
-          inspire a realistic belief in students’ respective individual
-          abilities;
-        </li>
-        <li>encourage, listen to and respect the student voice;</li>
-        <li>
-          recognise that gender is an issue within the sphere of learning;
-        </li>
-        <li>
-          assist students to develop as responsible learners and show tolerance
-          to and respect for each other and themselves;
-        </li>
-        <li>
-          be inclusive of parents and other stakeholders and form a partnership
-          in making decisions about each child’s learning;
-        </li>
-        <li>
-          invite parent/stakeholder voice and listen to their perspective and
-          opinion;
-        </li>
-        <li>be an active contributing member of the whole school community;</li>
-      </ul>
+      <div className="flex flex-wrap">
+        <div className="flex flex-col items-center justify-center max-w-sm mx-auto text-xl md:text-2xl w-full sm:w-1/2">
+          <div className="p-4 mb-8">
+            <h1>Our Approach</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+              corporis aliquam labore ratione id autem rerum accusantium, nihil
+              laborum dolore facilis cum vero quisquam vel magnam reprehenderit
+              et debitis quaerat!
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center px-4 md:p-8 w-full sm:w-1/2">
+          <img className="p-4 w-full" src={Image} alt="" />
+        </div>
+      </div>
+      <div className="max-w-3xl mx-auto py-8">
+        <div className="services">
+          <Service title="First">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+              corrupti suscipit, aliquid cum in odio ea quidem quis optio
+              similique voluptas praesentium. Harum iure distinctio eius.
+              Numquam iure laudantium commodi?
+            </p>
+          </Service>
+          <Service title="Second">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem illo
+              magni eos maxime incidunt ea quibusdam suscipit voluptatum
+              impedit? A excepturi sunt eius laudantium ut, tempore quibusdam
+              assumenda repellat. Quia.
+            </p>
+          </Service>
+          <Service title="Third">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              nam blanditiis commodi, perferendis reprehenderit consequatur
+              voluptatem nulla facere a nisi illo maxime eum soluta. Aspernatur
+              velit iure rerum impedit quam?
+            </p>
+          </Service>
+        </div>
+        <div className="services">
+          <Service title="Fourth">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis ut
+              vitae est alias assumenda voluptatum quia neque deserunt soluta
+              accusamus, facilis sint ea nisi non corporis consectetur id
+              recusandae mollitia.
+            </p>
+          </Service>
+          <Service title="Fifth">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
+              explicabo alias animi quidem dolor similique dolores sed ipsum
+              neque, accusantium cum aperiam harum! Atque, dicta reprehenderit
+              nihil sint blanditiis nulla?
+            </p>
+          </Service>
+          <Service title="Sixth">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+              nemo voluptatibus quas rem iure rerum doloremque obcaecati omnis
+              adipisci quo a, odio autem reprehenderit reiciendis atque quasi,
+              nobis explicabo deserunt!
+            </p>
+          </Service>
+        </div>
+      </div>
+      <Tagline />
     </Layout>
   );
 };
+
+const Service = props => (
+  <div className="max-w-sm mx-auto px-4 py-8 w-full">
+    <h2>{props.title}</h2>
+    {props.children}
+  </div>
+);
 
 export default OurApproach;
