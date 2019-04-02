19a57ea920980315fd292cd379e12c28f7008bd3
Luke Bennett <luke.bennett88@gmail.com>
2019-04-02 19:41:52 +1100

    Make font-weight lighter

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,19 +41,17 @@ const About = () => (
         At Aspiring Minds, students are provided with a positive and safe
         environment, with clear boundaries for discipline.
       </p>
-      <div className="font-sans max-w-md p-4 self-start text-grey-darkest text-xl">
-        <div>
-          <p>
-            We recognise the benefit of applying principles of consistency and
-            fairness within a framework of honesty, integrity and authenticity
-            which builds a respectful and tolerant atmosphere among the students
-            and between the students and staff.
-          </p>
-          <p>
-            The improvement in students’ self-esteem and confidence is extremely
-            gratifying to all involved.
-          </p>
-        </div>
+      <div className="font-normal font-sans max-w-md p-4 self-start text-grey-darkest text-xl">
+        <p>
+          We recognise the benefit of applying principles of consistency and
+          fairness within a framework of honesty, integrity and authenticity
+          which builds a respectful and tolerant atmosphere among the students
+          and between the students and staff.
+        </p>
+        <p>
+          The improvement in students’ self-esteem and confidence is extremely
+          gratifying to all involved.
+        </p>
       </div>
     </Skew>
 
