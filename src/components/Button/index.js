e7479cd3a6c40c969d65555635deef3ed6aad01a
Luke Bennett <luke.bennett88@gmail.com>
2019-04-02 19:40:11 +1100

    Make buttons bigger

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ const Button = props => (
   <p className="flex justify-center mt-8 w-full">
     <a
       href={props.link}
-      className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-4 py-2 relative rounded-full text-white text-sm md:text-lg tracking-wide"
+      className="btn gradient bg-blue-5 hover:bg-blue-4 flex font-display font-bold items-center justify-center no-underline px-8 py-2 relative rounded-full text-white text-sm sm:text-lg md:text-2xl tracking-wide"
     >
       <span className="flex mr-2">
         <img className="h-4 md:h-6 w-4 md:w-6" src={props.svg} alt="" />
