ab0ce40470ce3e819846bf2c098140dddc8a3c4a
Luke Bennett <luke.bennett88@gmail.com>
2019-04-12 09:38:34 +1000

    Add overflow hidden to prevent x-axis scroll

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,7 +10,7 @@ import '../../css/global.css';
 const Layout = ({ children }) => (
   <div
     dir="ltr"
-    className="bg-white flex flex-col font-sans max-w-4xl min-h-screen mx-auto text-grey-darkest w-full"
+    className="bg-white flex flex-col font-sans max-w-4xl min-h-screen mx-auto overflow-hidden text-grey-darkest w-full"
   >
     <Header />
     <main className="fade-in-up flex flex-col flex-1 mx-auto w-full">
