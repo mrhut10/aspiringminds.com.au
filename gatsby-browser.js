1bd68652e629689b255004ceb53beb42d5ac69aa
mrhut10 <mrhut10@protonmail.com>
2019-05-07 02:33:33 +1000

    deliberate lint error (should crash build)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 // Self host fonts using Typefaces.js https://github.com/KyleAMathews/typefaces
 
 // Load Poppins typeface
-require('typeface-poppins');
+require( 'typeface-poppins');
 
 // Load Quicksand typeface
 require('typeface-nunito');
